6a12b5857d1d96b884936ba01c92ce136fb12a63
tanuj.d <tanuj.d>
2020-03-29 21:33:37 +0530

    first commit

---
+++ client/build/precache-manifest.ac4e9d75f6209192d8fa0d2bddd03efd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbaad9c1f09d465a7032ac28baa2461f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b8068afb5a698f24a0a",
+    "url": "/static/css/main.bccd0344.chunk.css"
+  },
+  {
+    "revision": "fcb1029d74144164bee3",
+    "url": "/static/js/2.9632fcdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9632fcdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8068afb5a698f24a0a",
+    "url": "/static/js/main.d421f011.chunk.js"
+  },
+  {
+    "revision": "51a518e45330f669e104",
+    "url": "/static/js/runtime-main.51eec7e2.js"
+  }
+]);
